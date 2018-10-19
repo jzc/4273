363b296446464e58bfc3359a7a5fa6776e9d92ad
jzc <justincai.lps@gmail.com>
2018-10-19 00:01:49 -0600

    Small changes to webserver

--- pa2/webserver.c
+++ pa2/webserver.c
@@ -9,8 +9,9 @@
 #include <pthread.h>
 #include <signal.h>
 
-void handle_request(int clent);
 void sigint_handler(int sig);
+void *handle_request(void *clip);
+char *get_contenttype(char *filepath);
 
 int servsocket, clisocket;
 int main(int argc, char* argv[]) {
@@ -48,60 +49,13 @@ int main(int argc, char* argv[]) {
             return 1;
         }
         pthread_t thread;
-        if (pthread_create(&thread, NULL, (void * (*)(void *))handle_request, (void *)clisocket)) {
-            perror("Sad");
+        int *clip = malloc(sizeof clisocket);
+        *clip = clisocket;
+        if (pthread_create(&thread, NULL, handle_request, clip)) {
+            perror("pthread_create error");
         } 
+        // handle_request(clip);
     }
-}
-
-#define BUFSIZE 100000
-
-void handle_request(int client) {
-    char *recvbuf = malloc(BUFSIZE), *sendbuf = malloc(BUFSIZE);
-    char *token, *curr=recvbuf;
-    recv(client, recvbuf, BUFSIZE, 0);
-    if ((token = strsep(&curr, " ")) == NULL) {
-        return;
-    }
-    if (!strcmp(token, "GET")) {
-        // handle get
-        token = strsep(&curr, " ");
-        if (!strcmp(token, "/")) {
-            token = "/index.html";
-        }
-        char filepath[3+strlen(token)+1];
-        strcpy(filepath, "www");
-        strcat(filepath, token);
-
-        FILE *f = fopen(filepath, "rb");
-        if (f) {
-            // File found; 200
-            char *header = "HTTP/1.1 200 OK\r\n\r\n";
-            strsep(&curr, "."); 
-
-            send(client, header, strlen(header), 0);
-
-            fseek(f, 0, SEEK_END);
-            int size = ftell(f);
-            fseek(f, 0, SEEK_SET);
-
-            void *fmap = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fileno(f), 0);
-            send(client, fmap, size, 0);
-        } else {
-            // File not found; 404
-        }
-        close(client);
-
-
-    } else if (!strcmp(token, "POST")) {
-        // handle post
-    } else {
-        // handle other methods
-    }
-
-    free(recvbuf);
-    free(sendbuf);
-    return;
 }
 
 void sigint_handler(int sig) {
@@ -109,4 +63,92 @@ void sigint_handler(int sig) {
     close(servsocket);
     close(clisocket);
     exit(0);
+}
+
+#define BUFSIZE 2048
+
+void *handle_request(void *clip) {
+    int client = * (int *)clip;
+    char recvbuf[BUFSIZE], headerbuf[BUFSIZE], filepath[BUFSIZE];
+    char *token, *curr=recvbuf;
+    free(clip);
+
+    recv(client, recvbuf, BUFSIZE, 0);
+    if ((token = strsep(&curr, " ")) == NULL) {
+        return NULL;
+    }
+    if (!strcmp(token, "GET")) {
+        // handle get
+        token = strsep(&curr, " ");
+        printf("GET %s\n", token);
+        if (!strcmp(token, "/")) {
+            token = "/index.html";
+        }
+        sprintf(filepath, "www%s", token);
+
+        FILE *f = fopen(filepath, "rb");
+        if (f) {
+            // File found; 200
+
+            // Get size of file
+            fseek(f, 0, SEEK_END);
+            int size = ftell(f);
+            fseek(f, 0, SEEK_SET);
+
+            // Get content type
+            char *contenttype = get_contenttype(filepath);            
+
+            FILE *header = fmemopen(headerbuf, 1024, "w");
+            fprintf(header, "HTTP/1.1 200 OK\r\n");
+            fprintf(header, "Server: PA2 4273\r\n");
+            fprintf(header, "Content-Type: %s\r\n", contenttype);
+            fprintf(header, "Content-Length: %d\r\n", size);
+            fprintf(header, "\r\n");
+            fclose(header);
+            send(client, headerbuf, strlen(headerbuf), 0);
+
+            // Memory map file and send the rest of the response
+            void *fmap = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fileno(f), 0);
+            send(client, fmap, size, 0);
+            munmap(fmap, size);
+            fclose(f);
+        } else {
+            // File not found; 404
+        }
+    } else if (!strcmp(token, "POST")) {
+        // handle post
+    } else {
+        // handle other methods
+    }
+
+    close(client);
+    return NULL;
+}
+
+char *get_contenttype(char *filepath) {
+    char *p;
+    for (p = filepath; *p != '\0'; ++p);
+    for (; (*p != '.') && (p != filepath); --p);
+    if (!strcmp(p, ".html")) {
+        return "text/html";
+    }
+    if (!strcmp(p, ".txt")) {
+        return "text/plain";
+    }
+    if (!strcmp(p, ".png")) {
+        return "image/png";
+    }
+    if (!strcmp(p, ".gif")) {
+        return "image/gif";
+    }
+    if (!strcmp(p, ".png")) {
+        return "image/png";
+    }
+    if (!strcmp(p, ".css")) {
+        return "text/css";
+    }
+    if (!strcmp(p, ".js")) {
+        return "application/javascript";
+    }
+    return "text/plain";
 }
